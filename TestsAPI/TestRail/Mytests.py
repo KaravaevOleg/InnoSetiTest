1f2444cf6002bc5a3a509cebd5b7ef879bdd8533
o.karavaev <o.karavaev@systtech.ru>
2022-07-18 08:53:51 +0200

    remove

--- TestsAPI/TestRail/Mytests.py
+++
@@ -1,8 +0,0 @@
-import testrail
-
-
-client = testrail.APIClient('http:///testrail/')
-client.user = 'okaravaevm'
-client.password = 'wt/xIz7Zz.Su.Y3TEtuw'
-
-
--- TestsAPI/TestRail/pages/base_page.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class BasePage:
-    TITLE_PAGE = (By.XPATH, "//div[contains(@class,'k1zIA rSk4se')]//img[contains(@alt,'Google')]")
-    SEARCH_FIELD = (By.XPATH, "//input[contains(@title,'Поиск')]")
